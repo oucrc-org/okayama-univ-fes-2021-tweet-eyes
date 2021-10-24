b5598fc9af385a2cdc96f84a5221d12afc2627b3
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-10-24 21:59:15 +0900

    Add: Twitter API用

--- loadenv.py
+++ loadenv.py
@@ -21,6 +21,20 @@ def get_db_url(dir):
 def get_db_apikey():
     return os.getenv('DB_APIKEY')
 
+# チャンネルID
 def get_channel_id():
     return os.getenv('DISCORD_CHANNEL_ID')
-    
+
+# Twitter API
+def get_tw_api():
+    consumer_key = os.getenv('TW_CONSUMER_KEY')
+    consumer_secret = os.getenv('TW_CONSUMER_SECRET')
+    access_token = os.getenv('TW_ACCESS_TOKEN')
+    access_token_secret = os.getenv('TW_ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    return api
