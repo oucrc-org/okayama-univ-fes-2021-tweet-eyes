127f8dc788a0e4b61ed4c14b3709c23f37ecf018
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-10-26 09:06:49 +0900

    Add: pull_tweet.py

---
+++ pull_tweet.py
@@ -0,0 +1,22 @@
+import tweepy
+import loadenv
+
+def pull_tweet(tweet, id):
+    
+    return # 結果
+
+if __name__ == '__main__':
+    # ここにテストプログラムを書く
+
+    # ツイート内容を格納するクラス
+    # 引数はDBと同じ
+    class tweet:
+        def __init__(self, twitter_id, avatar_url, display_name, comment, tweet_url):
+            self.twitter_id = twitter_id
+            self.avatar_url = avatar_url
+            self.display_name = display_name
+            self.comment = comment
+            self.tweet_url = tweet_url
+
+
+    print('テスト')
